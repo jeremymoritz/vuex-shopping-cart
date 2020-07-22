d0895067b54e7d0f3b06db10d458560ab1c6518e
Jeremy Moritz <jeremy@jeremymoritz.com>
2020-07-22 08:54:04 -0500

    added prettierrc and some initial shopping cart features. kept most boilerplate intact for now

---
+++ src/components/Product.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="product-list">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>Product 1 goes here</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ProductList',
+  props: {
+    msg: String
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="products">
+    <img alt="Vue logo" src="../assets/shopping_cart.png" />
+    <ProductList msg="A List of Available Books" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductList from '@/components/ProductList.vue';
+
+export default {
+  name: 'Products',
+  components: {
+    ProductList
+  }
+};
+</script>
